e8e5256b16f6fe02dcf6e78c7a13aea87879d7f8
Aiden Gresh <agresh62006@gmail.com>
2026-04-14 19:40:30 -0400

    fdhjl

--- video_games_db.py
+++ video_games_db.py
@@ -1,5 +1,16 @@
 class VideoGames:
-    def __init__(self):
+    def __init__(self, Rank, Name, Platform, Year, Genre, Publisher, NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales):
+        self.Rank =  Rank
+        self.Name = Name
+        self.Platform = Platform
+        self.Year = Year
+        self.Genre = Genre
+        self.Publisher = Publisher
+        self.NA_sales = NA_Sales
+        self.EU_sales = EU_Sales
+        self.JP_Sales = JP_Sales
+        self.Other_Sales = Other_Sales,
+        self.Global_sales = Global_Sales
         pass
 
 import mysql.connector
