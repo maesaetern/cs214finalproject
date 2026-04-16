42d420b5279edbf12edb24124a031c6087d13f77
maesaetern <msaetern28@wooster.edu>
2026-04-16 17:29:05 -0400

    basics

--- video_games_db.py
+++ video_games_db.py
@@ -24,6 +24,24 @@ class VideoGameDB:
             database=database
         )
 
+    def search_by_prefix(self, prefix, limit=10):
+        # Search using LIKE
+        sql = "SELECT game_id, name, platform, year, genre, publisher FROM game WHERE name LIKE %s ORDER BY game_id LIMIT %s"
+        games = []
+        with self.conn.cursor() as cur:
+            cur.execute(sql, (prefix + "%", int(limit)))
+            for r in cur.fetchall():
+                games.append(VideoGame(r[1], r[2], r[3], r[4], r[5], r[6]))
+        return games
+
+    def get_video_game_by_name(self, name):
+        sql = "SELECT game_id, name, platform, year, genre, publisher FROM games WHERE rank = %s"
+        with self.conn.cursor(dictionary=True, buffered=True) as cur:
+            cur.execute(sql, (name,))
+            count = cur.rowcount
+            games = [VideoGame(r['name'], r['platform']) for r in cur.fetchall()]
+            return count, games
+
 def disconnect(self):
     self.conn.close()
 
